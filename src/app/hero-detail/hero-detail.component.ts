e33112f6516de97f0fd6f6dbee6ccac1d4812d0f
ngantt <nganthutran98@gmail.com>
2020-02-06 17:59:32 +0700

    .

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,46 +2,51 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HeroService } from '../hero.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { LocalstorageService } from '../localstorage.service';
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+    selector: 'app-hero-detail',
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  tuong : ApiModel.Tuong;
-  constructor(
-    private route: ActivatedRoute, 
-    private heroService: HeroService
-  ) { }
-
-  danhSachKyNang = ['Bão đạn', 'Ám khí', 'Lời nguyền tử vong', 'Hôn gió', 'Mê hoặc', 'Cái nhìn hóa đá'];
-  tuongForm: FormGroup;
-  ngOnInit() {   
-    return this.getTuong();
-  }
-
-  /**
-   * return a observable
-   */
-  getTuong() : void{
-    const tuongId = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getTuong(tuongId).subscribe(tuong => this.tuong = tuong);
+    tuong : ApiModel.Tuong;
+    constructor(
+        private route: ActivatedRoute, 
+        private heroService: HeroService,
+        private lsService: LocalstorageService
+        ) { 
+            this.lsService;
+        }
+        
+    danhSachKyNang = ['Bão đạn', 'Ám khí', 'Lời nguyền tử vong', 'Hôn gió', 'Mê hoặc', 'Cái nhìn hóa đá'];
+    tuongForm: FormGroup;
+    ngOnInit() {   
+        return this.getTuong();
+    }
     
-    this.tuongForm = new FormGroup({
-      'id' : new FormControl(this.tuong.id),
-      'ten' : new FormControl(this.tuong.ten),
-      'kyNang': new FormControl(this.tuong.kyNang),
-      'mau' : new FormControl(this.tuong.mau),
-      'anh' : new FormControl(this.tuong.anh)
-    });
-  }
-
-  updateTuong(tuong) : void {
-    const id = +this.route.snapshot.paramMap.get('id'); 
-    this.heroService.updateTuong(tuong);
-  }
-
-  deleteTuong(tuong){
-    this.heroService.deleteTuong(tuong);
-  }
+    /**
+    * return a observable
+    */
+    getTuong() : void{
+        const tuongId = +this.route.snapshot.paramMap.get('id');
+        this.heroService.getTuong(tuongId).subscribe(tuong => this.tuong = tuong);
+        
+        this.tuongForm = new FormGroup({
+            'id' : new FormControl(this.tuong.id),
+            'ten' : new FormControl(this.tuong.ten),
+            'kyNang': new FormControl(this.tuong.kyNang),
+            'mau' : new FormControl(this.tuong.mau),
+            'anh' : new FormControl(this.tuong.anh)
+        });
+    }
+    
+    updateTuong(tuong) : void {
+        const id = +this.route.snapshot.paramMap.get('id'); 
+        this.heroService.updateTuong(tuong);
+    }
+    
+    deleteTuong(tuong){
+        this.heroService.deleteTuong(tuong);
+    }
 }
+    
--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -2,67 +2,63 @@ import { Component, OnInit } from '@angular/core';
 import { TUONG } from '../danhSachTuong';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HeroService } from '../hero.service';
+import { LocalstorageService } from '../localstorage.service';
 
 @Component({
-  selector: 'app-hero-form',
-  templateUrl: './hero-form.component.html',
-  styleUrls: ['./hero-form.component.css']
+    selector: 'app-hero-form',
+    templateUrl: './hero-form.component.html',
+    styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent implements OnInit {
-  constructor(
-    private heroService: HeroService,
-    // private messageService: messageService
-  ) { }
-  json;
-  danhSachKyNang = [
-                    'Bão đạn', 'Ám khí', 'Lời nguyền tử vong', 'Hôn gió', 'Mê hoặc', 
-                    'Cái nhìn hóa đá', 'Siêu Bom Địa Ngục', 'Chuyển Hóa Năng Lượng'
-                  ];
+    json;
+    danhSachKyNang = [
+        'Bão đạn', 'Ám khí', 'Lời nguyền tử vong', 'Hôn gió', 'Mê hoặc', 
+        'Cái nhìn hóa đá', 'Siêu Bom Địa Ngục', 'Chuyển Hóa Năng Lượng'
+    ];
+    tuong = {id: 11, ten: 'Ziggs', kyNang: this.danhSachKyNang[7], mau: '689 (+0.5 mỗi cấp) ', anh: 'Ziggs'};
+    tuongForm: FormGroup;
+    message;
 
-  tuongForm: FormGroup;
-  message;
-  
-  tuong = {id: 11, ten: 'Ziggs', kyNang: this.danhSachKyNang[7], mau: '689 (+0.5 mỗi cấp) ', anh: 'Ziggs'};
-
-  ngOnInit(): void {
-    this.tuongForm = new FormGroup({
-      'id' : new FormControl(this.tuong.id),
-      'ten' : new FormControl(this.tuong.ten),
-      'kyNang': new FormControl(this.tuong.kyNang),
-      'mau' : new FormControl(this.tuong.mau),
-      'anh' : new FormControl(this.tuong.anh),
-    }); 
-  }
-
-  danhSachTuong = TUONG;
-
-  tuong1 = this.danhSachTuong[0];
-  
-  get id() {
-    return this.tuongForm.get('id'); }
-
-  get ten() { return this.tuongForm.get('ten'); }
-  get kyNang() { return this.tuongForm.get('kyNang'); }
-  get mau() { return this.tuongForm.get('mau'); }
-  get anh() { return this.tuongForm.get('anh'); }
-
-  submitted = false;
-
-  onSubmit() { this.submitted = true; }
-
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.tuong)};
-
-  add(tuong) : void {  
-    this.heroService.addTuong(tuong).subscribe(value => console.log(value));
-  }
-
-  private resetForm() {
-    this.tuongForm.reset();
-  }
-  
-  private log(message: string){
-
-  }
-
+    constructor(
+        private heroService: HeroService,
+        private lsService: LocalstorageService
+        ) 
+    {
+        this.lsService.storageAvailable();
+    }
+        
+    ngOnInit(): void {
+        this.tuongForm = new FormGroup({
+            'id' : new FormControl(this.tuong.id),
+            'ten' : new FormControl(this.tuong.ten),
+            'kyNang': new FormControl(this.tuong.kyNang),
+            'mau' : new FormControl(this.tuong.mau),
+            'anh' : new FormControl(this.tuong.anh),
+        }); 
+    }
+    
+    danhSachTuong = TUONG;
+    
+    get id() { return this.tuongForm.get('id'); }
+    get ten() { return this.tuongForm.get('ten'); }
+    get kyNang() { return this.tuongForm.get('kyNang'); }
+    get mau() { return this.tuongForm.get('mau'); }
+    get anh() { return this.tuongForm.get('anh'); }
+    
+    submitted = false;
+    
+    onSubmit() { this.submitted = true; }
+    
+    // TODO: Remove this when we're done
+    get diagnostic() { return JSON.stringify(this.tuong)};
+    
+    add(tuong) : void {  
+        this.heroService.addTuong(tuong).subscribe(value => console.log(value));
+    }
+    private resetForm() {
+        this.tuongForm.reset();
+    }
+    private log(message: string){
+    }
 }
+        
